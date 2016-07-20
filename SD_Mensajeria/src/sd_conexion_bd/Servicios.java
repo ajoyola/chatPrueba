42d875226c7340cbddd53d27ac50ea667b5c7d15
JoycePC <JoycePC@hpe014>
2016-07-20 05:44:11 -0500

    Requerimientos y creacion de la conexion con la base de datos

---
+++ SD_Mensajeria/src/sd_conexion_bd/Servicios.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sd_conexion_bd;
+
+/**
+ *
+ * @author JoycePC
+ */
+
+import java.awt.Rectangle;
+import java.sql.SQLException;
+
+public class Servicios extends SQLQuery{ 
+    //CONSULTAS A LA BASE
+    
+}
